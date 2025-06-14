716e0ebcdbe3d8ceebcfedc3909835d38bab8fe1
Nguyễn Văn Thành <thanhnguyen76bk@gmail.com>
2025-06-14 11:41:16 +0700

    [Project]- Havent yet checked with reality

--- 02.Designpattern/Creational/Factory/main.c
+++ 02.Designpattern/Creational/Factory/main.c
@@ -11,21 +11,6 @@ int main() {
     //     free(tempSensor);
     // }
 
-    // // Create a humidity sensor
-    // Sensor* humiditySensor = createSensor(SENSOR_HUMIDITY);
-    // if (humiditySensor != NULL) {
-    //     humiditySensor->init();
-    //     printf("Humidity: %.2f%%\n", humiditySensor->readData());
-    //     free(humiditySensor);
-    // }
-
-    // // Create a light sensor
-    // Sensor* lightSensor = createSensor(SENSOR_LIGHT);
-    // if (lightSensor != NULL) {
-    //     lightSensor->init();
-    //     printf("Light Intensity: %.2f lux\n", lightSensor->readData());
-    //     free(lightSensor);
-    // }
 
     return 0;
 }
